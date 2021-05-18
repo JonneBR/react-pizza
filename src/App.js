2d57c988366a5aa4d9dc95e3280feec14128bca7
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2021-05-18 14:15:26 -0300

    navbar styled

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import { GlobalStyle } from './GlobalStyles';
 
 function App() {
-  return <div>Hello React</div>;
+  return (
+    <Router>
+      <GlobalStyle />
+      <Navbar />
+    </Router>
+  );
 }
 
 export default App;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Bars, Nav, NavIcon, NavLink } from './NavbarElements';
 
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <>
+      <Nav>
+        <NavLink to='/'>Pizza</NavLink>
+        <NavIcon>
+          <p>Menu</p>
+          <Bars />
+        </NavIcon>
+      </Nav>
+    </>
+  );
 };
 
 export default Navbar;
