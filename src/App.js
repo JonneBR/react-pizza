300328ae39cdfcc438018ec9167c1bbf4c7f1bc2
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2021-05-21 11:46:31 -0300

    feature and footer components and style

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 import Hero from './components/Hero/Hero';
 import Products from './components/Products/Products';
-import { productData } from './components/Products/data';
+import { productData, productDataTwo } from './components/Products/data';
 
 import { GlobalStyle } from './GlobalStyles';
+import Feature from './components/Feature/Feature';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
@@ -12,6 +14,9 @@ function App() {
       <GlobalStyle />
       <Hero />
       <Products heading='Choose your favorite' data={productData} />
+      <Feature />
+      <Products heading='Sweet Treats for You' data={productDataTwo} />
+      <Footer />
     </Router>
   );
 }
